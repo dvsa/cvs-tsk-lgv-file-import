573fa5e2ff125911d5dc443ec4d6d8fd5d2ef83b
Nathan Brown <nathancjb@googlemail.com>
2022-06-22 13:43:38 +0100

    CB2-4347: Update S3 Events to loop (#11)
    * feat(cb2-4347): Create loop to process multiple S3 events and test
    
    * feat(cb2-4347): Remove unused import
    
    * feat(cb2-4347): Update error logging
    
    * feat(cb2-4347): Remove unneeded error block

--- src/handler/s3Event.ts
+++ src/handler/s3Event.ts
@@ -1,10 +1,24 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
 import * as fs from 'fs';
-import type { S3Event } from 'aws-lambda';
+import type { S3Event, S3EventRecord } from 'aws-lambda';
 import { configureFile } from '../fileConvert/fileConvert';
 import { filePull } from '../filePull/fromS3';
 import { filePush } from '../filePush/filePush';
 import { randomUUID } from 'crypto';
+import logger from '../util/logger';
+
+const handleEvent = async (record: S3EventRecord) => {
+  const workingDir = `/tmp/${randomUUID()}/`;
+  try {
+    fs.mkdirSync(workingDir);
+    const evlFileData = await filePull(record);
+    const filepath = await configureFile(workingDir, evlFileData.data, evlFileData.filename);
+    await filePush(filepath);
+  } finally {
+    fs.rmSync(workingDir, { recursive: true, force: true });
+  }
+
+};
 
 /**
  * Lambda Handler
@@ -16,15 +30,17 @@ import { randomUUID } from 'crypto';
 export const handler = async (
   event: S3Event,
 ): Promise<Record<string, unknown>> => {
-  const workingDir = `/tmp/${randomUUID()}/`;
-  try {
-    fs.mkdirSync(workingDir);
-    const record = event.Records[0];
-    const evlFileData = await filePull(record);
-    const filepath = await configureFile(workingDir, evlFileData.data, evlFileData.filename);
-    await filePush(filepath);
-  } finally {
-    fs.rmSync(workingDir, { recursive: true, force: true });
+
+  for (const record of event.Records) {
+    try {
+      await handleEvent(record);
+    } catch  (err) {
+      logger.error(err);
+      logger.error(`The file ${record.s3.object.key} failed somewhere, 500 was returned.`);
+      return Promise.resolve({
+        statusCode: 500,
+      });
+    }
   }
 
   return Promise.resolve({
--- tests/handler/s3Event.test.ts
+++ tests/handler/s3Event.test.ts
@@ -25,6 +25,7 @@ jest.mock('ssh2-sftp-client', () => {
 });
 
 import event from '../resources/s3event.json';
+import eventTwo from '../resources/s3eventTwo.json';
 import type { S3Event } from 'aws-lambda';
 import { GetObjectOutput } from 'aws-sdk/clients/s3';
 import { handler } from '../../src/handler/s3Event';
@@ -48,11 +49,66 @@ describe('Test S3 Event Lambda Function', () => {
       ContentType: 'text/csv',
       Body: Buffer.from('File content'),
     };
-    mockS3.promise.mockResolvedValueOnce(getObjectOutput);
+    mockS3.promise.mockResolvedValue(getObjectOutput);
     const eventMock: S3Event = event as S3Event;
 
     const res: Record<string, unknown> = await handler(eventMock);
 
     expect(res.statusCode).toBe(204);
+  });
+
+  test('should return 204 with multiple s3 events', async () => {
+    jest.spyOn(filePush, 'createConfig').mockImplementation(() => {
+      const config = {
+        host: process.env.SFTP_Host,
+        username: process.env.SFTP_User,
+        retries: 3,
+        password: 'testPassword',
+      };
+      return config;
+    });
+    mockConnect.mockReturnValue(Promise.resolve(true));
+    mockFastPut.mockReturnValue(Promise.resolve('uploaded'));
+    mockEnd.mockReturnValue(Promise.resolve(void 0));
+    const getObjectOutput: GetObjectOutput = {
+      ContentType: 'text/csv',
+      Body: Buffer.from('File content'),
+    };
+    mockS3.promise.mockResolvedValue(getObjectOutput);
+    const eventMock: S3Event = eventTwo as S3Event;
+
+    const res: Record<string, unknown> = await handler(eventMock);
+
+    expect(res.statusCode).toBe(204);
+  });
+
+  test('should return 500 with multiple s3 events if one breaks', async () => {
+    jest.spyOn(filePush, 'createConfig').mockImplementation(() => {
+      const config = {
+        host: process.env.SFTP_Host,
+        username: process.env.SFTP_User,
+        retries: 3,
+        password: 'testPassword',
+      };
+      return config;
+    });
+    mockConnect.mockReturnValue(Promise.resolve(true));
+    mockFastPut.mockReturnValue(Promise.resolve('uploaded'));
+    mockEnd.mockReturnValue(Promise.resolve(void 0));
+    const getObjectOutput: GetObjectOutput = {
+      ContentType: 'text/csv',
+      Body: Buffer.from('File content'),
+    };
+    const getObjectOutputBroken: GetObjectOutput = {
+      ContentType: 'text',
+      Body: Buffer.from('File content'),
+    };
+    mockS3.promise.mockResolvedValueOnce(getObjectOutput);
+    mockS3.promise.mockResolvedValueOnce(getObjectOutputBroken);
+    const eventMock: S3Event = eventTwo as S3Event;
+
+    const res: Record<string, unknown> = await handler(eventMock);
+
+    expect(res.statusCode).toBe(500);
   });
 });
