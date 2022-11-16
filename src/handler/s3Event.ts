a4b104b7e3b7c5c5478a8f4f75da0dcda51ab95c
Steven.Pears <steven.pears@live.co.uk>
2022-11-16 15:57:32 +0000

    initial S3 event rewire

--- src/handler/s3Event.ts
+++ src/handler/s3Event.ts
@@ -1,71 +1,49 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
-import * as fs from 'fs';
-import type { S3Event, S3EventRecord } from 'aws-lambda';
-import { configureEvlFile } from '../fileConvert/fileConvert';
+import type { S3Event } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
 import { filePull } from '../filePull/fromS3';
-import { filePush } from '../filePush/filePush';
-import { randomUUID } from 'crypto';
 import logger from '../util/logger';
+import * as XLSX from 'xlsx';
 
-const handleEvlEvent = async (record: S3EventRecord) => {
-  const workingDir = `/tmp/evl/${randomUUID()}/`;
-  try {
-    fs.mkdirSync(workingDir, { recursive: true });
-    const evlFileData = await filePull(record);
-    const filepath = await configureEvlFile(
-      workingDir,
-      evlFileData.data,
-      evlFileData.filename,
-    );
-    await filePush(filepath, 'evl');
-  } finally {
-    fs.rmSync(workingDir, { recursive: true, force: true });
-  }
-};
+const range = (start:number, end:number):number[] => Array.from({ length: (end + 1 - start) }, (v, k) => k + start);
+const getValue = (sheet:XLSX.WorkSheet, row:number, column: number):string => (sheet[XLSX.utils.encode_cell({ r:row, c:column })] as XLSX.CellObject).v as string;
+const sqsClient = new AWS.SQS();
 
-const handleTflEvent = async (record: S3EventRecord) => {
-  const workingDir = `/tmp/tfl/${randomUUID()}/`;
-  try {
-    fs.mkdirSync(workingDir, { recursive: true });
-    const tflFileData = await filePull(record);
-    const filepath = workingDir + tflFileData.filename;
-    fs.writeFileSync(filepath, tflFileData.data);
-    await filePush(filepath, 'tfl');
-  } finally {
-    fs.rmSync(workingDir, { recursive: true, force: true });
-  }
-};
-
-/**
- * Lambda Handler
- *
- * @param {S3Event} event
- * @returns {Promise<string>}
- */
 export const handler = async (event: S3Event): Promise<string> => {
   logger.debug(`event: ${JSON.stringify(event, null, 2)}`);
 
   for (const record of event.Records) {
     try {
-      const fileName = record.s3.object.key;
+      const lgvExcelFile = await filePull(record);
+      const workbook = XLSX.read(lgvExcelFile.data);
 
-      if (fileName.startsWith('EVL_') && process.env.EVL_SFTP_SEND === 'true') {
-        await handleEvlEvent(record);
-      } else if (
-        fileName.startsWith('VOSA') &&
-        process.env.TFL_SFTP_SEND === 'true'
-      ) {
-        await handleTflEvent(record);
-      } else {
-        logger.info('Did not send to SFTP server, check the env vars');
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const sheetRange = XLSX.utils.decode_range(worksheet['!ref']);
+
+      for (const rowNumber of range(sheetRange.s.r + 1, sheetRange.e.r)) {
+        try {
+          const model = {
+            'application': getValue(worksheet, rowNumber, 0),
+            'vin': getValue(worksheet, rowNumber, 1),
+            'vrm': getValue(worksheet, rowNumber, 2),
+            'trl': getValue(worksheet, rowNumber, 3),
+            'class': getValue(worksheet, rowNumber, 4),
+            'cycle': getValue(worksheet, rowNumber, 5),
+            'cc': getValue(worksheet, rowNumber, 6),
+          };
+
+          const msgResult = await sqsClient.sendMessage({
+            QueueUrl:process.env.QUEUE_URL,
+            MessageBody:JSON.stringify(model),
+          }).promise();
+          console.log(model);
+        } catch (err) {
+
+        }
       }
+      return `All rows of ${record.s3.object.key} processed successfully.`;
     } catch (err) {
-      logger.error('', err);
-      return Promise.reject(
-        `The file ${record.s3.object.key} errored during processing.`,
-      );
+      return `The file ${record.s3.object.key} errored during processing.`;
     }
   }
-
-  return Promise.resolve('All records processed successfully.');
 };
